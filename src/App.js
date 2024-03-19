d1c11970c620d483d533ad6231c9fa1aef9211f1
manhcuong1502222 <acuongsh@gmail.com>
2024-03-19 15:41:48 +0700

    done-topbar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scences/global/Topbar";
 
 function App() {
+
+  const [theme, colorMode] = useMode();
+
   return (
     <div className="app">
 
-
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <main className="content">
+              <Topbar />
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
 
     </div>
   );
